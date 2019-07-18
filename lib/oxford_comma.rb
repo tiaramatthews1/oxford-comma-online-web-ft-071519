fb8b49c34cf30115190f99d40260913f150256de
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-07-18 19:24:35 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,8 @@ def oxford_comma(array)
   return array.first if array.size == log10
   
   if array.size == 2
-    array.insert(1, "and").join("")
+    array.insert(1, "and").join(" ")
+    
 end
 
 def oxford_comma(array)
