b6df1a729465606c867789acedb7cb87b4b68fe4
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-07-18 19:26:38 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,8 @@ def oxford_comma(array)
   
   if array.size == 2
     array.insert(1, "and").join(" ")
-  elseif array.size > 2
+  elsif array.size > 2
     array[-1].insert(0, "and")
     array.join(", ")
+  end
 end
