0548d084d2ea43bb70b8e659559cfacac20c1300
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-07-18 19:25:59 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,8 @@ def oxford_comma(array)
   if array.size == 2
     array.insert(1, "and").join(" ")
   elseif array.size > 2
-    array[-1].insert(0, " and ")
+    array[-1].insert(0, " and")
+    array.join 
 end
 
 def oxford_comma(array)
